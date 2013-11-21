f4afd894dc80e14f830a8af28819cd3c56faff8c
Andrey Derevyagin <2derand@gmail.com>
2013-11-22 00:02:01 +0200

    Fri Nov 22 00:02:01 EET 2013

--- src/kanojo_info.py
+++ src/kanojo_info.py
@@ -11,7 +11,8 @@ except ImportError:
 	print 'Error import module "settings", see settings.py.template'
 	sys.exit(1)
 KANOJOS_INFO = getattr(settings, 'KANOJOS_INFO', {})
-SELECTED_USER_ID = getattr(settings, 'SELECTED_USER_ID', {})
+SELECTED_USER_ID = getattr(settings, 'SELECTED_USER_ID', 0)
+FRIENDS_USER_IDS = getattr(settings, 'FRIENDS_USER_IDS', [])
 from libkanojo import Kanojo, KanojoInfo
 
 
@@ -35,10 +36,14 @@ if __name__=='__main__':
 		s = ''
 		if info.steady_id == SELECTED_USER_ID:
 			s = '\033[1;32m%d\033[00m'%k['ean']
-		elif not SELECTED_USER_ID in info.followers_ids:
-			s = '\033[1;31m%d\033[00m'%k['ean']
-		else:
+		elif info.steady_id in FRIENDS_USER_IDS or k.has_key('steady') and k['steady'] == info.steady_id:
+			#s = '\033[1;36m%d\033[00m'%k['ean']
 			s = '%d'%k['ean']
-		print '\t%s\t%s\t%s'%(s, info.name, info.steady_name)
-	
-
+		else:
+			s = '\033[1;31m%d\033[00m'%k['ean']
+		if info.steady_id == SELECTED_USER_ID or SELECTED_USER_ID in info.followers_ids:
+			s += '\033[1;32m [OK]\033[00m'
+		else:
+			s += '\033[1;31m [nFOUND]\033[00m'
+		print '\t%s\t\033[1;33m%s\033[00m\t%s'%(s, info.name, info.steady_name)
+		
