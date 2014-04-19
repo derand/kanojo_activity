97ecbefbfa3b109df7a96f1069611c2812a592f3
Andrey Derevyagin <2derand@gmail.com>
2014-04-20 00:02:02 +0300

    Sun Apr 20 00:02:01 EEST 2014

--- src/gdrive_cdn.py
+++ src/gdrive_cdn.py
@@ -21,8 +21,8 @@ class UploadToCDN(object):
 	def upload(self, file_path_or_url):
 		curl = pycurl.Curl()
 		#curl.setopt(pycurl.URL, 'http://gdrive-cdn.herokuapp.com/upload')
-		#curl.setopt(pycurl.URL, 'http://cdn.derand.net/upload')
-		curl.setopt(pycurl.URL, 'http://localhost:5000/upload')
+		curl.setopt(pycurl.URL, 'http://cdn.derand.net/upload')
+		#curl.setopt(pycurl.URL, 'http://localhost:5000/upload')
 		curl.setopt(pycurl.POST, 1)
 		if os.path.isfile(file_path_or_url):
 			curl.setopt(pycurl.HTTPPOST, [('file', (pycurl.FORM_FILE, file_path_or_url))])
