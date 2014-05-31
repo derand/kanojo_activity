bb6486fc0f80660e6fee04f08664c438594e8efb
Andrey Derevyagin <2derand@gmail.com>
2014-05-31 13:02:02 +0300

    Sat May 31 13:02:02 EEST 2014

--- src/gdrive_cdn.py
+++ src/gdrive_cdn.py
@@ -12,23 +12,35 @@ class UploadToCDN(object):
 
 	def getLink(self, json_str):
 		try:
-			print json_str
 			tmp = json.loads(json_str)
 		except ValueError:
 			return False
 		return tmp.get('url', False)
 
-	def upload(self, file_path_or_url):
+	def upload(self, file_path_or_url, content_type='image/jpeg', filename='file.jpg'):
 		curl = pycurl.Curl()
 		curl.setopt(pycurl.URL, 'http://gdrive-cdn.herokuapp.com/upload')
 		#curl.setopt(pycurl.URL, 'http://cdn.derand.net/upload')
 		#curl.setopt(pycurl.URL, 'http://localhost:5000/upload')
 		curl.setopt(pycurl.POST, 1)
-		if os.path.isfile(file_path_or_url):
+		isFile = False
+		try:
+			isFile = os.path.isfile(file_path_or_url)
+		except TypeError, e:
+			pass
+		if isFile:
 			curl.setopt(pycurl.HTTPPOST, [('file', (pycurl.FORM_FILE, file_path_or_url))])
 		elif 'http' == file_path_or_url[:4]:
 			#curl.setopt(pycurl.HTTPPOST, [('url', file_path_or_url), ('key', self.key)])
 			curl.setopt(pycurl.HTTPPOST, [('url', file_path_or_url)])
+		else:
+			data = [
+				('method', 'post'),
+				('key', '---jZ3d5AlEHMouqesM7MXiVYEhZQjo8GfjiQ'),
+				('file"; filename="%s'%filename,
+					(pycurl.FORM_CONTENTS, file_path_or_url, pycurl.FORM_CONTENTTYPE, content_type))
+			]
+			curl.setopt(pycurl.HTTPPOST, data)
 		buf = StringIO()
 		curl.setopt(pycurl.WRITEFUNCTION, buf.write)
 		try:
